50941156b82d07f18ac00627c5817f48124a8a03
patrick brisbin <pbrisbin@gmail.com>
2016-01-23 12:01:41 -0500

    Add --debug and --region options

--- src/Popeye/CLI.hs
+++ src/Popeye/CLI.hs
@@ -1,24 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Popeye.CLI (run) where
 
 import Popeye.Users
 
+import Control.Lens (set)
 import Control.Monad.IO.Class
+import Data.List (intercalate)
 import LoadEnv
 import Network.AWS
 import Options.Applicative
+import System.IO (stderr)
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-data Options = Options { oGroup :: Group }
+deriving instance Bounded Region
+deriving instance Enum Region
+
+data Options = Options
+    { oGroup :: Group
+    , oRegion :: Region
+    , oDebug :: Bool
+    }
 
 run :: IO ()
 run = do
     loadEnv
 
-    env <- newEnv NorthVirginia Discover
     opts <- getOptions
+    lgr <- newLogger (if oDebug opts then Debug else Error) stderr
+    env <- set envLogger lgr <$> newEnv (oRegion opts) Discover
 
     runResourceT . runAWS env $
         (mapM_ outputUser =<< getUsers (oGroup opts))
@@ -36,10 +49,26 @@ getOptions = execParser $ info (helper <*> parseOptions)
 
 parseOptions :: Parser Options
 parseOptions = Options
-    <$> Group . T.pack
-    <$> strOption
-        (  long "group"
-        <> short 'g'
+    <$> (Group . T.pack <$> strOption
+        (  short 'g'
+        <> long "group"
         <> metavar "GROUP"
         <> help "IAM group to query"
+        ))
+    <*> option auto
+        (  short 'r'
+        <> long "region"
+        <> metavar "REGION"
+        <> help ("One of " <> intercalate ", " (map show regions))
+        <> value NorthVirginia
+        <> showDefaultWith show
         )
+    <*> switch
+        (  short 'd'
+        <> long "debug"
+        <> help "Show debug information"
+        )
+
+  where
+    regions :: [Region]
+    regions = [minBound .. maxBound]
